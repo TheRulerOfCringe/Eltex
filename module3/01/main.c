f0ce8ccb53755d14751f9072dc2fafa08d445779
root <root@UbuntuMate1.myguest.virtualbox.org>
2025-11-17 19:12:35 +0700

    Added 3 module 01 task

--- module3/01/main.c
+++ module3/01/main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <ctype.h>  // для isdigit()
 #include <unistd.h>
+#include <stdlib.h>
 
 bool is_int(const char* str)
 {
@@ -54,19 +55,61 @@ int main(int argc, char *argv[])
 	{
         printf("Arg %d: \"%s\". Is_int = %d. Is_float = %d.\n", i, argv[i], is_int(argv[i]), is_float(argv[i]));
     }
+    printf("WHO PRINT THAT?\n");
+    int mid = (argc+1)/2;
 	pid_t pid;
 	pid = fork();
-	if (pid > 0)
+	if (pid < 0)
 	{
-		; // родительский процесс
-	}
-	else if (pid == 0)
-	{
-		; // дочерний процесс
+	    printf("Error, pid < 0!!");
 	}
 	else
 	{
-		;
+	    if (pid > 0)
+	    {
+		    for (int i = 1; i < mid; i++)
+		    {
+		        //printf("parent, %d %s\n", i, argv[i]); // родительский процесс
+		        if (is_int(argv[i]))
+		        {
+		            int tmp = atoi(argv[i]);
+		            printf("paretn said: %d) %d, %d\n", i, tmp, tmp*2);
+		        }
+		        else if (is_float(argv[i]))
+		        {
+		            float tmp = atof(argv[i]);
+		            printf("paretn said: %d) %f, %f\n", i, tmp, tmp*2);
+		        }
+		        else
+		        {
+		            printf("paretn said: %d) %s\n", i, argv[i]);
+		        }
+		    }
+	    }
+	    else if (pid == 0)
+	    {
+		    for (int i = mid; i < argc; i++)
+		    {
+		        if (is_int(argv[i]))
+		        {
+		            int tmp = atoi(argv[i]);
+		            printf("child said: %d) %d, %d\n", i, tmp, tmp*2);
+		        }
+		        else if (is_float(argv[i]))
+		        {
+		            float tmp = atof(argv[i]);
+		            printf("child said: %d) %f, %f\n", i, tmp, tmp*2);
+		        }
+		        else
+		        {
+		            printf("child said: %d) %s\n", i, argv[i]);
+		        }
+		    }
+	    }
+	    else
+	    {
+		    printf("Error!");
+	    }
+	    printf("\nMy pid is %d\n", pid);
 	}
-	printf("%d", pid);
 }
